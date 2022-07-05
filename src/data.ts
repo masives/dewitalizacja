960d6506e3590a21f68b471451575195fd4e316f
Jozef Piecyk <jozollo@gmail.com>
2022-07-05 17:49:49 +0200

    add Rawicz

--- src/data.ts
+++ src/data.ts
@@ -4,6 +4,8 @@ import parczewAfter from './assets/images/parczew_after.jpg';
 import parczewBefore from './assets/images/parczew_before.jpeg';
 import bartoszyceAfter from './assets/images/bartoszyce_after.jpg';
 import bartoszyceBefore from './assets/images/bartoszyce_before.jpg';
+import rawiczAfter from './assets/images/rawicz_after.jpg';
+import rawiczBefore from './assets/images/rawicz_before.png';
 
 interface Entry {
   title: string;
@@ -48,6 +50,15 @@ export const cityData: Entry[] = [
         'https://www.facebook.com/polskabezdrzew/posts/pfbid025eqDT6BnATYuYFnmqWUo7ACiPU5vXz9p8Q7gGPsfELz6pYeQeeShHG3Tq5Sdbskcl?__cft__[0]=AZUy66ixQZR_8rxaWW1wnOsw5krCqtqA72ahx0vwrHwOtmtq7orxinovJd1yFrQjzxCotwF5Bcc6ZDN-wFSerCjwtVkXyRRmlPCMI3o7eXyyUV12Ed108xZ206m7AzYrRLOxI3BAPlO26XBfWPdxR5I3F6Ma3LchJauAgaaTcsxIe_SHxrN0kF9M2LHvUEFoX0dGpf_BIgkrjTHdEhSUP81JXUW-eUkPru1vx9-X3cRDKQ&__tn__=%2CO%2CP-R',
       afterSourceUrl:
         'https://www.facebook.com/polskabezdrzew/posts/pfbid025eqDT6BnATYuYFnmqWUo7ACiPU5vXz9p8Q7gGPsfELz6pYeQeeShHG3Tq5Sdbskcl?__cft__[0]=AZUy66ixQZR_8rxaWW1wnOsw5krCqtqA72ahx0vwrHwOtmtq7orxinovJd1yFrQjzxCotwF5Bcc6ZDN-wFSerCjwtVkXyRRmlPCMI3o7eXyyUV12Ed108xZ206m7AzYrRLOxI3BAPlO26XBfWPdxR5I3F6Ma3LchJauAgaaTcsxIe_SHxrN0kF9M2LHvUEFoX0dGpf_BIgkrjTHdEhSUP81JXUW-eUkPru1vx9-X3cRDKQ&__tn__=%2CO%2CP-R',
+    },
+  },
+  {
+    title: 'Rawicz rynek',
+    photos: {
+      after: rawiczAfter,
+      before: rawiczBefore,
+      beforeSourceUrl: 'https://smoglab.pl/przeglad-rewitalizacji-rynkow/',
+      afterSourceUrl: 'https://smoglab.pl/przeglad-rewitalizacji-rynkow/',
     },
   },
 ];
