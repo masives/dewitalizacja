fb4c5270b07daf5453abceb7a2b0cb7de17c6b7d
Jozef Piecyk <jozollo@gmail.com>
2022-07-05 17:35:32 +0200

    add parczew

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,7 @@
 import skierniewiceAfter from './assets/images/skierniewice_after.jpeg';
 import skierniewiceBefore from './assets/images/skierniewice_before.jpeg';
+import parczewAfter from './assets/images/parczew_after.jpg';
+import parczewBefore from './assets/images/parczew_before.jpeg';
 
 interface Entry {
   title: string;
@@ -18,13 +20,21 @@ interface Entry {
 
 export const cityData: Entry[] = [
   {
-    title: 'Skierniwice',
-
+    title: 'Skierniwice rynek',
     photos: {
       after: skierniewiceAfter,
       before: skierniewiceBefore,
       beforeSourceUrl: 'https://twitter.com/JanMencwel/status/1138194072106078208',
       afterSourceUrl: 'https://twitter.com/JanMencwel/status/1138194072106078208',
+    },
+  },
+  {
+    title: 'Parczew rynek',
+    photos: {
+      after: parczewAfter,
+      before: parczewBefore,
+      beforeSourceUrl: 'https://twitter.com/aval4nche/status/480051858854391808',
+      afterSourceUrl: 'Facebook.com/kierunekwschod26/',
     },
   },
 ];
