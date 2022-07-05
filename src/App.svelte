92afb92468b882679870d99110cd9504faf7e6c9
Jozef Piecyk <jozollo@gmail.com>
2022-07-05 17:54:39 +0200

    remove h1 size

--- src/App.svelte
+++ src/App.svelte
@@ -73,11 +73,9 @@
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
-    font-size: 4rem;
     font-weight: 100;
     line-height: 1.1;
     margin: 2rem auto;
-    max-width: 14rem;
   }
 
   p {
