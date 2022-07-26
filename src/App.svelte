50478c703fc5bcaa29b78220458ee42759b78870
Jozef Piecyk <jozollo@gmail.com>
2022-07-26 22:32:59 +0200

    add analytics

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,24 @@
   import Contact from './Contact.svelte';
   import { Router, Route, Link } from 'svelte-navigator';
   import Home from './Home.svelte';
+
+  // <!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+  (function (f, a, t, h, o, m) {
+    a[h] =
+      a[h] ||
+      function () {
+        (a[h].q = a[h].q || []).push(arguments);
+      };
+    (o = f.createElement('script')), (m = f.getElementsByTagName('script')[0]);
+    o.async = 1;
+    o.src = t;
+    o.id = 'fathom-script';
+    m.parentNode.insertBefore(o, m);
+  })(document, window, '//dewfit.bieda.it/tracker.js', 'fathom');
+
+  window.fathom('set', 'siteId', 'KAQJK');
+  window.fathom('trackPageview');
+  // <!-- / Fathom -->
 </script>
 
 <Router>
