21fffd87b3100887962f6fe27b0148b79d48df60
Jozef Piecyk <jozollo@gmail.com>
2022-08-02 22:13:36 +0200

    add cookie modal, load analytics only when confirmed

--- src/App.svelte
+++ src/App.svelte
@@ -2,24 +2,7 @@
   import Contact from './Contact.svelte';
   import { Router, Route, Link } from 'svelte-navigator';
   import Home from './Home.svelte';
-
-  // <!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
-  (function (f, a, t, h, o, m) {
-    a[h] =
-      a[h] ||
-      function () {
-        (a[h].q = a[h].q || []).push(arguments);
-      };
-    (o = f.createElement('script')), (m = f.getElementsByTagName('script')[0]);
-    o.async = 1;
-    o.src = t;
-    o.id = 'fathom-script';
-    m.parentNode.insertBefore(o, m);
-  })(document, window, '//dewfit.bieda.it/tracker.js', 'fathom');
-
-  window.fathom('set', 'siteId', 'KAQJK');
-  window.fathom('trackPageview');
-  // <!-- / Fathom -->
+  import Analytics from './Analytics.svelte';
 </script>
 
 <Router>
@@ -43,6 +26,7 @@
 <footer>
   © 2022 <a href={'https://github.com/masives/dewitalizacja#jak-doda%C4%87-wpis'} target="_blank" rel="noopener noreferrer">Józef Piecyk</a>
 </footer>
+<Analytics />
 
 <style global>
   :root {
@@ -68,7 +52,8 @@
     text-align: center;
   }
 
-  nav a {
+  nav a,
+  button {
     color: var(--primary-color);
     text-decoration: none;
     padding: 8px;
